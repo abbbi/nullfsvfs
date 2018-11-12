fd2aad8dd2abd1d224ce2853a3a0116107dfdda8
abi <abi@grinser.de>
2018-11-12 08:16:48 -0800

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,23 @@
+dmesg -c > /dev/null
 umount /my/
 rmmod nullfs
 insmod nullfs.ko
 
 mount -t nullfs none /my/
 
+cp /etc/fstab /my/fstab
+stat -c'%n      size: %s blocks:%b' /etc/fstab
+stat -c'%n      size: %s blocks:%b' /my/fstab
+
+mkdir /tmp/testdir 
+mkdir /my/testdir
+stat -c'%n      size: %s blocks:%b' /tmp/testdir
+stat -c'%n      size: %s blocks:%b' /my/testdir
+
+
 for FILE in `echo /my/file /tmp/file`; do
 	dd if=/dev/zero of=$FILE bs=1024 count=2000>/dev/null 2>&1
 	stat -c'%n	size: %s blocks:%b' $FILE
 done
+
+dmesg| tail -n 5
